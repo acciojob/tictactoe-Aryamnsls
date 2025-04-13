225e3e516f06ace3b5349f0c1f36a9b2d3727efa
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2025-04-13 14:53:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ document.addEventListener("DOMContentLoaded", () => {
   let board = Array(9).fill("");
 
   const winConditions = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // cols
-    [0, 4, 8], [2, 4, 6]             // diagonals
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
   ];
 
   function checkWin() {
